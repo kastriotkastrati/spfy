55f5dde7ef95b5cca18b2eb2e378edfd44cb0f55
Kastriot Kastrati <kastriotrkastrati@gmail.com>
2023-02-05 20:28:34 +0100

    start: example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use rspotify;
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+use rspotify::{self, prelude::BaseClient};
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let creds = rspotify::Credentials::from_env().unwrap();
+    let spotify = rspotify::ClientCredsSpotify::new(creds);
+
+    spotify.request_token().await.unwrap();
+
+    // Running the requests
+    let birdy_uri =
+        rspotify::model::AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
+    let albums = spotify.album(birdy_uri).await;
+
+    println!("Response: {albums:#?}");
 }
 
 fn establish_connection() {}
