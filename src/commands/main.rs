ebd101eff838f441459f08df3e0c912e136cad08
Kastriot Kastrati <kastriotrkastrati@gmail.com>
2023-09-24 00:58:34 +0200

    fix: add apple-script to set wallpaper

--- src/commands/main.rs
+++ src/commands/main.rs
@@ -1,8 +1,9 @@
 use std;
-struct MacOS;
+pub struct MacOS;
 
-trait SpotifyInformer {
+pub trait SpotifyInformer {
     fn get_spotify_data(&self) -> String;
+    fn set_wallpaper(&self, path: &std::path::Path) -> ();
 }
 
 impl SpotifyInformer for MacOS {
@@ -24,15 +25,29 @@ impl SpotifyInformer for MacOS {
         let data = String::from_utf8_lossy(&output.stdout).to_string();
         return data;
     }
+
+    fn set_wallpaper(&self, path: &std::path::Path) -> () {
+        let path = path.to_str().unwrap();
+        let set_desktop_picture = format!("set desktop picture to POSIX file \"{}\"", path);
+        let script = [
+            "tell application \"Finder\"",
+            set_desktop_picture.as_str(),
+            "end tell",
+        ];
+
+        let script_construction = script.iter().flat_map(|&part| ["-e", part]);
+        let _ = std::process::Command::new("osascript")
+            .args(script_construction)
+            .output()
+            .expect("Could not run osascript.");
+    }
 }
 
-pub fn run_command(os: &str) -> String {
-    println!("os: {:?}", os);
-    let os: &dyn SpotifyInformer = match os {
+pub fn get_impl_commands<'a>(os: &'a str) -> &'a dyn SpotifyInformer {
+    let os_impl: &dyn SpotifyInformer = match os {
         "macos" => &MacOS,
         _ => panic!(""),
     };
 
-    let spotify_data = os.get_spotify_data();
-    return spotify_data;
+    return os_impl;
 }
--- src/spfy.rs
+++ src/spfy.rs
@@ -13,32 +13,55 @@ fn save_file_to_fs(
     return Ok(destination_file);
 }
 
-async fn download_image(image_url: &str, setup: setup::SetupStruct) {
+async fn download_image(
+    image_url: &str,
+    setup: setup::SetupStruct,
+) -> Result<String, reqwest::Error> {
     let client = reqwest::Client::new();
-    let response = client.get(image_url).send().await;
-    let response = response.unwrap();
-    let image_bytes = response.bytes().await.unwrap();
+    let response = client.get(image_url).send().await?;
+    let content_type = response
+        .headers()
+        .get(reqwest::header::CONTENT_TYPE)
+        .and_then(|v| v.to_str().ok());
+
+    let extension = match content_type {
+        Some("image/jpeg") => "jpg",
+        Some("image/png") => "png",
+        Some("image/gif") => "gif",
+        Some("image/bmp") => "bmp",
+        Some("image/webp") => "webp",
+        _ => "unknown",
+    };
 
     let image_name = nanoid::nanoid!();
+    let image_name = format!("{}.{}", image_name, extension);
     let image_path = {
         let mut path_buff = setup.dir_path.to_path_buf();
         path_buff.push(image_name);
         path_buff
     };
 
-    let image_file = save_file_to_fs(&image_path, image_bytes);
+    let image_bytes = response.bytes().await.unwrap();
+    let _ = save_file_to_fs(&image_path, image_bytes)
+        .expect("Could not save the image of the current song on your machine:(");
 
-    println!("image_file: {:?}", image_file);
+    let image_path = image_path.to_string_lossy().to_string();
+    return Ok(image_path);
 }
 
 pub async fn run_program() -> () {
     let setup_data = setup::setup();
     let os = std::env::consts::OS;
-    let image_url_form_command = commands::main::run_command(os);
-    let image_url_cleaned: String = image_url_form_command
+    let impl_commands = commands::main::get_impl_commands(os);
+
+    let image_url_from_impl = impl_commands.get_spotify_data();
+    let image_url_cleaned: String = image_url_from_impl
         .chars()
         .filter(|c| !c.is_whitespace())
         .collect();
 
-    let _ = download_image(&image_url_cleaned, setup_data).await;
+    let saved_image_url = download_image(&image_url_cleaned, setup_data).await;
+    let saved_image_url = saved_image_url.expect("Couldn't save the image:(");
+    let saved_image_path = std::path::Path::new(&saved_image_url);
+    let _ = impl_commands.set_wallpaper(saved_image_path);
 }
